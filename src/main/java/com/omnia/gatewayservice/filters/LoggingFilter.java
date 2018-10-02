14b6b717c1f24f652bac62aa47f767e4634e3c7d
Santos92 <AlexanderLundbergSantos@live.se>
2018-10-02 22:08:19 +0200

    first commit

---
+++ src/main/java/com/omnia/gatewayservice/filters/LoggingFilter.java
@@ -0,0 +1,36 @@
+package com.omnia.gatewayservice.filters;
+
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+@Order(1)
+public class LoggingFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(
+            ServletRequest request,
+            ServletResponse response,
+            FilterChain chain) throws IOException, ServletException {
+
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+
+}
